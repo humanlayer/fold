625dfc45104a7fdaf3bcf0fd36bb03f9b66c96bf
Kyle Mistele <kyle@humanlayer.dev>
2026-07-03 00:02:56 -0700

    chore(tart-core): export HookRunner modules

--- packages/tart-core/src/index.ts
+++ packages/tart-core/src/index.ts
@@ -1,7 +1,15 @@
 export * from './Ids.ts'
+
+/** Event Log exports */
 export * from './EventLog/Errors.ts'
 export * from './EventLog/LogEntryFactory.ts'
 export * from './EventLog/MemoryLayer.ts'
 export * from './EventLog/EventLogService.ts'
 export * from './EventLog/Schemas.ts'
+
+/** projection exports that transform log into other useful things */
 export * from './Projection/Projection.ts'
+
+/** Hook Runner exports */
+export * from './HookRunner/HookRunnerService.ts'
+export * from './HookRunner/Schema.ts'
